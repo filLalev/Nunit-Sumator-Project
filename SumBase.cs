7db4b31667f73fd0e3bcb24b2fb1bc1419696697
filLalev <115897851+filLalev@users.noreply.github.com>
2022-11-20 13:42:15 +0200

    Initial_comit

---
+++ SumBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Sumator
+{
+    public static class SumBase
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = arr[0];
+            for (int i = 1; i<arr.Length; i++)
+            { sum+=arr[i]; }
+            return sum;
+        }
+    }
+}
